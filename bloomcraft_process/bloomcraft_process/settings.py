ec08e811b53e55234c59956ca84beb95e8c9a682
Stuart Anderson <emblem@gmail.com>
2017-07-04 07:12:23 +0000

    pull debug from environment

--- bloomcraft_process/bloomcraft_process/settings.py
+++ bloomcraft_process/bloomcraft_process/settings.py
@@ -21,7 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+try :
+    DEBUG = os.environ['DEBUG']
+except KeyError:
+    DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 if DEBUG:
